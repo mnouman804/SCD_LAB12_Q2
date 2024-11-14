c7325775b8163ddc082d00eb60f15fe2c065428a
mnouman804 <f223637@cfd.nu.edu.pk>
2024-11-14 11:28:31 +0500

    bug

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ package lab12git;
 public class main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the application!");
-		greetUser();
+		//greetUser();
 	}
 
 	public static void greetUser() {
