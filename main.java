77eb35c5ae88138d9a2ef6dcf38d27a492e833f7
mnouman804 <f223637@cfd.nu.edu.pk>
2024-11-14 11:25:23 +0500

    initial commit

---
+++ main.java
@@ -0,0 +1,12 @@
+package lab12git;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the application!");
+		greetUser();
+	}
+
+	public static void greetUser() {
+		System.out.println("Hello, User!");
+	}
+}
